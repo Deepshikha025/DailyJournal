b4b592f956df3dde976f271b067fcef30ef77123
Deepshikha025 <91478605+Deepshikha025@users.noreply.github.com>
2021-12-18 20:53:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _= require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "We welcome you to the Blog Spot and hope that you've an amazing time writing and expressing your thoughts.We look forward that you write about yourself and your thoughts to help and inspire others, who so ever comes across it. Have an amazing time ahead!! ";
+const aboutContent = "We are a team of full of people who not only wants good for you but the best and we hope that we provide you an easy way to express your thoughts on this website. We have developed this website only 2 years back and starting from mere 35 people of team, we are now a family of five lakh people.";
+const contactContent = "You are most welcome for your queries and can contact us at anytime. Solving your issues is our job and we look forward to a healthy relationship with you all. The contact numbers are: 9898989898, you can also mail your issues faced on the mail-id: blogspotxyz@gmail.com";
 
 const app = express();
 
@@ -71,3 +71,4 @@ app.get("/posts/:postName", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
